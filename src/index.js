4c62dec659705b29d20b7dd7ab90b5b8eb323d27
DaisyChepyegon <daisychepyegon@gmail.com>
2022-06-18 14:39:40 +0300

    fetch the data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 // Code here
+document.addEventListener("DOMContentLoaded", () => {
+  function getData() {
+    fetch("http://localhost:3000/beers")
+      .then((resp) => resp.json())
+      .then((data) => listBeer(data));
+  }
+
+  function listBeer(data) {
+    for (const names of data) {
+      const unList = document.querySelector("#beer-list");
+      const list= document.createElement("li");
+      unList.dataset.id = names.id;
+      list.innerHTML = `
+        <h5>${names.name}</h5>
+         `;
+      ulList.append(list);
+    }
+  }
+  
+  getData();
+
+});
