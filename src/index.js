c9b52eed7777263c569d7c2e6506604ae24950c0
DaisyChepyegon <daisychepyegon@gmail.com>
2022-06-20 07:43:17 +0300

    get beer reviews

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
 // Code here
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  function getData() {
-    fetch("http://localhost:3000/beers")
-      .then((resp) => resp.json())
-      .then((data) => listBeer(data))
-      .then((data) => addDescription(data))
-  }
-  
-  function listBeer(data) {
-    for (const names of data) {
-      const unList = document.querySelector("#beer-list");
-      const list= document.createElement("li");
-      unList.dataset.id = names.id;
-      list.innerHTML = `
-        <h5>${names.name}</h5>
-         `;
-      unList.append(list);
-    }
-  }
-  
-  getData();
 
-  function addDescription(data){
-
-  }
+  fetchData();
 
 });
+//fetch beers
+function fetchData() {
+  fetch("http://localhost:3000/beers")
+    .then((resp) => resp.json())
+    .then((data) => listBeers(data));
+}
+//Find the container where we attach everything to
+const beerUl = document.querySelector("#beer-list");
+let beerName = document.getElementById("beer-name");
+let beerImage = document.getElementById("beer-image");
+let beerDescription = document.getElementById("beer-description");
+let beerReviews =document.getElementById("review-list")
+function listBeers(data) {
+  data.forEach((data) => {
+    const list = document.createElement("li");
+    list.innerText = data.name;
+
+    beerUl.appendChild(list);
+    list.addEventListener("click", () => {
+      beerName.textContent = data.name;
+      beerImage.setAttribute("src", data.image_url);
+      beerDescription.textContent = data.description;
+      beerReviews.textContent = data.reviews;
+    });
+  });
+}
